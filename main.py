cc6b737c39a81407cedd31590fa6c8d800d4d81f
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-12-11 00:03:47 -0800

    ✨ feat(blogs): implement get_blog_comments and get_blog_comment_by_id functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 
 class BlogTypes(str, Enum):
@@ -17,8 +18,10 @@ async def root():
 
 
 @app.get("/blogs")
-async def get_blogs():
-    return {"message": "This action returns all blogs!"}
+async def get_blogs(skip: int = 0, limit: int = 5, type: Optional[BlogTypes] = None):
+    return {
+        "message": f"This action returns all blogs! Skip: {skip} - Limit: {limit} - Type: {type.value if type is not None else type}"
+    }
 
 
 @app.get("/blogs/{blog_id}")
@@ -26,6 +29,15 @@ async def get_blog_by_id(blog_id: int):
     return {"message": f"This action returns #{blog_id} blog!"}
 
 
-@app.get("/blogs/type/{blog_type}")
-async def get_blog_by_type(blog_type: BlogTypes):
-    return {"message": f"This action returns a {blog_type.value} blog!"}
+@app.get("/blogs/{blog_id}/comments")
+async def get_blog_comments(blog_id: int, username: Optional[str] = None):
+    return {
+        "message": f"This action returns #{blog_id} blog includes all comments! Username: {username}"
+    }
+
+
+@app.get("/blogs/{blog_id}/comments/{comment_id}")
+async def get_blog_comment_by_id(blog_id: int, comment_id: int, valid: bool = True):
+    return {
+        "message": f"This action returns #{blog_id} blog includes #{comment_id} comment! Valid: {valid}"
+    }
