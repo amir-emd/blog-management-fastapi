9f75de002761a4ec9705c56c6f7862f4492e73ee
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-12-10 20:59:14 -0800

    ✨ feat(blogs): implement get_blog_by_id and get_blogs functions

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/blogs")
+async def get_blogs():
+    return {"message": "This action returns all blogs!"}
+
+
+@app.get("/blogs/{blog_id}")
+async def get_blog_by_id(blog_id: int):
+    return {"message": f"This action returns #{blog_id} blog!"}
